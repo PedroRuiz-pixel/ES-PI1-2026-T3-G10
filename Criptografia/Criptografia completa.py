af00fc207375629d788d32d3b118c640fa7e7cf8
Matheus <maaugusto443@gmail.com>
2026-05-12 19:00:34 -0300

    Add Hill cipher implementation in Python

---
+++ Criptografia/Criptografia completa.py
@@ -0,0 +1,88 @@
+ALFABETO = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+MODULO = len(ALFABETO)
+
+
+def limpar_texto(texto):
+    texto = texto.upper()
+    resultado = ""
+
+    for caractere in texto:
+        if caractere in ALFABETO:
+            resultado += caractere
+
+    return resultado
+
+
+def texto_para_numeros(texto):
+    numeros = []
+
+    for caractere in texto:
+        numeros.append(ALFABETO.index(caractere))
+
+    return numeros
+
+
+def numeros_para_texto(numeros):
+    texto = ""
+
+    for numero in numeros:
+        texto += ALFABETO[numero % MODULO]
+
+    return texto
+
+
+def multiplicar_matriz_vetor(matriz, vetor):
+    resultado_1 = (matriz[0][0] * vetor[0] + matriz[0][1] * vetor[1]) % MODULO
+    resultado_2 = (matriz[1][0] * vetor[0] + matriz[1][1] * vetor[1]) % MODULO
+
+    return [resultado_1, resultado_2]
+
+
+def cifrar_hill(texto):
+    chave = [
+        [5, 7],
+        [2, 3]
+    ]
+
+    texto = limpar_texto(texto)
+
+    if len(texto) % 2 != 0:
+        texto += "X"
+
+    numeros = texto_para_numeros(texto)
+    criptografado = []
+
+    for i in range(0, len(numeros), 2):
+        bloco = [numeros[i], numeros[i + 1]]
+        bloco_cifrado = multiplicar_matriz_vetor(chave, bloco)
+        criptografado.extend(bloco_cifrado)
+
+    return numeros_para_texto(criptografado)
+
+
+def decifrar_hill(texto):
+    chave_inversa = [
+        [3, 29],
+        [34, 5]
+    ]
+
+    texto = limpar_texto(texto)
+
+    numeros = texto_para_numeros(texto)
+    descriptografado = []
+
+    for i in range(0, len(numeros), 2):
+        bloco = [numeros[i], numeros[i + 1]]
+        bloco_decifrado = multiplicar_matriz_vetor(chave_inversa, bloco)
+        descriptografado.extend(bloco_decifrado)
+
+    return numeros_para_texto(descriptografado)
+
+
+mensagem = input("Digite a mensagem: ")
+
+mensagem_cifrada = cifrar_hill(mensagem)
+print("Mensagem cifrada:", mensagem_cifrada)
+
+mensagem_decifrada = decifrar_hill(mensagem_cifrada)
+print("Mensagem decifrada:", mensagem_decifrada)
